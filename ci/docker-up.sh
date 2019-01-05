79354dc3b4c402f7b56f72609a1f5003692411b1
abhilash singh <daft300punk@gmail.com>
2019-01-05 12:06:38 +0530

    Update

--- ci/docker-up.sh
+++ ci/docker-up.sh
@@ -18,7 +18,7 @@ for i in `seq 1 600`; do
 	# wait for containers to come up
 	sleep 1
 	# get a response from backend and frontend
-	curl localhost:48010 >/dev/null 2>&1 && curl localhost:48001 >/dev/null 2>&1
+	curl localhost:8080 >/dev/null 2>&1
 	# exit if successful
 	if [ $? -eq 0 ]; then
 		kill $pid
